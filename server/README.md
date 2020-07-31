eb53b4570128c8fc76ca03b8b278006687baa0ae
Deep512 <deepdhanuka.512@gmail.com>
2020-07-31 12:39:25 +0530

    final files

---
+++ server/README.md
@@ -0,0 +1,42 @@
+## DBMS Setup
+##### Installing MySQL
+```
+$ sudo apt update
+$ sudo apt install mysql-server
+```
+
+#### Creating a new user
+```MySQL
+$ sudo mysql -u root
+mysql> CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';
+mysql> GRANT ALL PRIVILEGES ON * . * TO 'root'@'localhost';
+```
+
+#### Creating the database and tables
+Login to mysql as user root
+```
+$ mysql -u root -p
+```
+Enter password as `root` <br>
+Run all the commands in `/SQL_Queries/DatabaseDefinition.sql` by running
+```MySQL
+mysql> source /DatabaseDefinition.sql
+```
+Or alternatively copy and paste the commands.
+
+## Running the server
+#### Prerequites
+- `nodejs`
+- Node modules. Can be installed using `npm`, with the command `npm install <module>`
+  - `express`
+  - `mysql`
+  - `get-json`
+  - `nodemon`
+  - `body-parser`
+
+#### To run the server
+Move to the `/PortfolioManagement/` directory and run the following command
+```
+$ npm start
+```
+This will get the server code running on `localhost:3000`
---
+++ server/server.js
@@ -0,0 +1,549 @@
+// ################################################################### MySQL
+
+var mysql = require("mysql");
+var config = require("./config");
+
+var dbms = mysql.createConnection(config);
+
+var pool = mysql.createPool(config);
+
+dbms.connect(function (err) {
+	if (err) throw err;
+
+	dbms.query("USE Portfolio;", function (err, result) {
+		if (err) throw err;
+		else {
+			console.log(">> Successfully connected to Portfolio Database");
+		}
+	});
+});
+
+// ################################################################# Server
+var bcrypt = require("bcrypt");
+const saltRounds = 10;
+var express = require("express");
+var app = express();
+var getJSON = require("get-json");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
+app.use(bodyParser.json());
+app.use(cors());
+
+// List of users
+app.get("/", function (req, res) {
+	var query = "SELECT * from User";
+	dbms.query(query, function (err, result) {
+		res.json(result);
+	});
+});
+
+// Login
+app.post("/login", function (req, res) {
+	// Get login details of user
+	var query = "CALL loginDetails(?)";
+	dbms.query(query, req.body.username, function (err, result, fields) {
+		if (err) throw err;
+		if (result[0].length == 0) {
+			res.status(400).json("Invalid username");
+		}
+
+		bcrypt.compare(req.body.password, result[0][0]["Password"], function (
+			err,
+			resu
+		) {
+			if (resu == false) {
+				res.status(400).json("Wrong Password");
+			} else {
+				res.json(result[0][0]);
+			}
+		});
+	});
+});
+
+// Register
+app.post("/register", function (req, res) {
+	// Check if User exists in database
+	var query = `CALL checkUser(?)`;
+	dbms.query(query, req.body.username, function (err, result, fields) {
+		if (err) throw err;
+		if (req.body.username === "") {
+			res.status(400).json("empty username");
+		} else if (result[0].length === 1) {
+			res.status(400).json("username exists");
+		} else if (req.body.firstname === "") {
+			res.status(400).json("empty firstname");
+		} else if (req.body.password === "") {
+			res.status(400).json("empty password");
+		} else if (req.body.password !== req.body.confirm_password) {
+			res.status(400).json("unmatching password");
+		} else {
+			pool.getConnection(function (err, connection) {
+				if (err) {
+					connection.release();
+					throw err;
+				}
+				connection.query("USE Portfolio;", function (err, result) {
+					if (err) {
+						connection.release();
+						throw err;
+					}
+				});
+				connection.beginTransaction(function (err) {
+					if (err) {
+						connection.release();
+						throw err;
+					}
+					bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+						var insertQuery = `CALL insertUser(?,?,?,?)`;
+						connection.query(
+							insertQuery,
+							[req.body.username, hash, req.body.firstname, req.body.lastname],
+							function (err, result, fields) {
+								if (err) {
+									connection.rollback(function () {
+										connection.release();
+										throw err;
+									});
+								}
+								var IDquery = "CALL getId(?)";
+								connection.query(IDquery, req.body.username, function (
+									err,
+									result,
+									fields
+								) {
+									if (err) {
+										connection.rollback(function () {
+											connection.release();
+											throw err;
+										});
+									}
+									connection.commit(function (err) {
+										if (err) {
+											connection.rollback(function () {
+												connection.release();
+												throw err;
+											});
+										}
+										res.json(result[0][0]);
+										console.log("success!");
+										connection.release();
+									});
+								});
+							}
+						);
+					});
+				});
+			});
+		}
+	});
+});
+
+// Profile
+app.post("/profile", function (req, res) {
+	// Details of all the shares bought
+	var bought = "CALL bought(?)";
+
+	dbms.query(bought, req.body.id, function (err, bght, fields) {
+		if (err) throw err;
+		var bought;
+		var invested;
+		if (bght[0].length == 0) {
+			bought = 0;
+			invested = 0.0;
+		} else {
+			bought = bght[0][0]["bought"];
+			invested = bght[0][0]["invested"];
+		}
+		// Details of all the shares sold
+		var sold = "CALL sold(?)";
+		dbms.query(sold, req.body.id, function (err, sld, fields) {
+			if (err) throw err;
+			var sold;
+			var returns;
+			if (sld[0].length == 0) {
+				sold = 0;
+				returns = 0.0;
+			} else {
+				sold = sld[0][0]["sold"];
+				returns = sld[0][0]["returns"];
+			}
+
+			var current = Number(bought - sold);
+			// Get user info FROM UserID
+			var userInfoQuery = "CALL userInfo(?)";
+
+			dbms.query(userInfoQuery, req.body.id, function (err, userInfo, fields) {
+				const val = {
+					current: current,
+					invested: Number(invested),
+					returns: Number(returns),
+					username: userInfo[0][0]["Username"],
+					firstname: userInfo[0][0]["FirstName"],
+					lastname: userInfo[0][0]["LastName"],
+				};
+				res.json(val);
+			});
+		});
+	});
+});
+
+// Watch List
+app.post("/watch-list", function (req, res) {
+	// Stocks in WatchList of a user
+	var query = "CALL watchList(?)";
+	dbms.query(query, req.body.id, function (err, result, fields) {
+		if (err) throw err;
+		res.json(result[0]);
+	});
+});
+
+// Stock List
+app.get("/stock-list", function (req, res) {
+	// List of all the Stocks
+	var query = "CALL stockList";
+	dbms.query(query, function (err, result, fields) {
+		if (err) throw err;
+		res.json(result[0]);
+	});
+});
+
+// Stock page
+app.post("/share", function (req, res) {
+	getJSON(
+		"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" +
+			req.body.symbol +
+			"&interval=1min&apikey=YOUR_API_KEY",
+		function (err, data) {
+			if (err) throw err;
+			if (data["Error Message"]) {
+				res.status(400).json("Unable to fetch data");
+			} else {
+				var first = 1;
+				var price;
+				for (var key in data["Time Series (1min)"]) {
+					if (first == 1) {
+						price = data["Time Series (1min)"][key]["4. close"];
+						first = 0;
+					}
+					// Insert into ShareHistory
+					var historyQuery = "CALL shareHistory(?,?,?)";
+					dbms.query(
+						historyQuery,
+						[key, req.body.symbol, data["Time Series (1min)"][key]["4. close"]],
+						function (err, result, fields) {}
+					);
+				}
+
+				// Get Share Info for symbol
+				var query = "CALL shareInfo(?)";
+				dbms.query(query, req.body.symbol, function (err, resultname, fields) {
+					if (err) throw err;
+					// Check if share exists in WatchList
+					var existsQuery = "CALL inWatchList(?,?)";
+					dbms.query(existsQuery, [req.body.id, req.body.symbol], function (
+						err,
+						result,
+						fields
+					) {
+						if (err) throw err;
+
+						// Details of Shares bought of specific company
+						var bought = "CALL boughtShare(?,?)";
+
+						dbms.query(bought, [req.body.id, req.body.symbol], function (
+							err,
+							bght,
+							fields
+						) {
+							if (err) throw err;
+							var bought;
+							var invested;
+							if (bght[0].length == 0) {
+								bought = 0;
+								invested = 0.0;
+							} else {
+								bought = bght[0][0]["bought"];
+								invested = bght[0][0]["invested"];
+							}
+							// Details of Shares sold of specific company
+							var sold = "CALL soldShare(?,?)";
+							dbms.query(sold, [req.body.id, req.body.symbol], function (
+								err,
+								sld,
+								fields
+							) {
+								if (err) throw err;
+								var sold;
+								var returns;
+								if (sld[0].length == 0) {
+									sold = 0;
+									returns = 0.0;
+								} else {
+									sold = sld[0][0]["sold"];
+									returns = sld[0][0]["returns"];
+								}
+								var current = Number(bought - sold);
+								var xdata = [],
+									y = [];
+								for (x in data["Time Series (1min)"]) {
+									xdata.push(x);
+									y.push(Number(data["Time Series (1min)"][x]["4. close"]));
+								}
+								var val = {
+									name: resultname[0][0]["Sharename"],
+									info: resultname[0][0]["Information"],
+									inWatchList: result[0].length > 0,
+									current: current,
+									invested: Number(invested),
+									returns: Number(returns),
+									price: price,
+									xdata: xdata,
+									y: y,
+								};
+								res.json(val);
+							});
+						});
+					});
+				});
+			}
+		}
+	);
+});
+
+// Buy share
+app.post("/buy", function (req, res) {
+	var now = new Date();
+	var timestamp =
+		now.toISOString().substr(0, 10) + " " + now.toISOString().substr(11, 8);
+
+	// Insert into BuyShare
+	var buyQuery = "CALL buyQuery(?,?,?,?,?)";
+
+	dbms.query(
+		buyQuery,
+		[
+			timestamp,
+			req.body.id,
+			req.body.quantity,
+			req.body.price,
+			req.body.symbol,
+		],
+		function (err, result, fields) {
+			if (err) throw err;
+
+			// Insert into User History
+			var historyQuery = "call userHistory(?,?,?,?,?,?)";
+
+			dbms.query(
+				historyQuery,
+				[
+					timestamp,
+					req.body.id,
+					req.body.quantity,
+					req.body.price,
+					0,
+					req.body.symbol,
+				],
+				function (err, result, fields) {
+					if (err) throw err;
+					res.json(result);
+				}
+			);
+		}
+	);
+});
+
+// Sell share
+app.post("/sell", function (req, res) {
+	var now = new Date();
+	var timestamp =
+		now.toISOString().substr(0, 10) + " " + now.toISOString().substr(11, 8);
+
+	// Insert into SellShare
+	var sellQuery = "CALL sellQuery(?,?,?,?,?)";
+
+	dbms.query(
+		sellQuery,
+		[
+			timestamp,
+			req.body.id,
+			req.body.quantity,
+			req.body.price,
+			req.body.symbol,
+		],
+		function (err, result, fields) {
+			if (err) throw err;
+			// Insert into User History
+			var historyQuery = "CALL userHistory(?,?,?,?,?,?)";
+			dbms.query(
+				historyQuery,
+				[
+					timestamp,
+					req.body.id,
+					req.body.quantity,
+					req.body.price,
+					1,
+					req.body.symbol,
+				],
+				function (err, result, fields) {
+					if (err) throw err;
+					res.json(result);
+				}
+			);
+		}
+	);
+});
+
+// Add to watch list
+app.post("/add", function (req, res) {
+	// Insert into Watch list
+	var query = "CALL insertList(?,?)";
+
+	dbms.query(query, [req.body.id, req.body.symbol], function (
+		err,
+		result,
+		fields
+	) {
+		if (err) throw err;
+		res.json(result);
+	});
+});
+
+// Remove from watch list
+app.post("/remove", function (req, res) {
+	pool.getConnection(function (err, connection) {
+		if (err) {
+			connection.release();
+			throw err;
+		}
+		connection.query("USE Portfolio;", function (err, result) {
+			if (err) {
+				connection.release();
+				throw err;
+			}
+		});
+		connection.beginTransaction(function (err) {
+			if (err) {
+				connection.release();
+				throw err;
+			}
+			// Delete from Watch list
+			var query = "CALL deleteList(?,?)";
+			connection.query(query, [req.body.id, req.body.symbol], function (
+				err,
+				result,
+				fields
+			) {
+				if (err) {
+					connection.rollback(function () {
+						connection.release();
+						throw err;
+					});
+				}
+				// Stocks in WatchList of a user
+				var query = "CALL watchList(?)";
+				connection.query(query, req.body.id, function (err, result, fields) {
+					if (err) {
+						connection.rollback(function () {
+							connection.release();
+							throw err;
+						});
+					}
+					connection.commit(function (err) {
+						if (err) {
+							connection.rollback(function () {
+								connection.release();
+								throw err;
+							});
+						}
+						res.json(result[0]);
+						console.log("success!");
+						connection.release();
+					});
+				});
+			});
+		});
+	});
+});
+
+// Update Profile
+app.post("/update-profile", function (req, res) {
+	// Get user info FROM UserID
+	var userInfoQuery = `CALL userInfo(?)`;
+	dbms.query(userInfoQuery, req.body.id, function (err, result, fields) {
+		if (err) throw err;
+		const value = {
+			username: result[0][0]["Username"],
+			firstname: result[0][0]["FirstName"],
+			lastname: result[0][0]["LastName"],
+		};
+		res.json(value);
+	});
+});
+
+// Update Profile
+app.post("/save", function (req, res) {
+	// Check if user exists with same username
+	var query = "CALL updateCheck(?,?)";
+	dbms.query(query, [req.body.username, req.body.id], function (
+		err,
+		result,
+		fields
+	) {
+		if (err) throw err;
+		// Get password of User
+		var currentPasswordQuery = "CALL getPassword(?)";
+		dbms.query(currentPasswordQuery, req.body.id, function (err, pass, fields) {
+			if (err) throw err;
+			bcrypt.compare(req.body.old_password, pass[0][0]["Password"], function (
+				err,
+				resu
+			) {
+				if (err) throw err;
+				if (req.body.username == "") {
+					res.status(400).json("empty username");
+				} else if (result[0].length == 1) {
+					res.status(400).json("username exists");
+				} else if (req.body.firstname == "") {
+					res.status(400).json("empty firstname");
+				} else if (resu == false) {
+					res.status(400).json("incorrect old password");
+				} else if (req.body.new_password === "") {
+					res.status(400).json("empty new password");
+				} else if (req.body.new_password !== req.body.confirm_password) {
+					res.status(400).json("unmatching password");
+				} else {
+					bcrypt.hash(req.body.new_password, saltRounds, function (err, hash) {
+						// Update details of a user
+						var updateQuery = "CALL updateUser(?,?,?,?,?)";
+						dbms.query(
+							updateQuery,
+							[
+								req.body.username,
+								hash,
+								req.body.firstname,
+								req.body.lastname,
+								req.body.id,
+							],
+							function (err, result, fields) {
+								if (err) throw err;
+								res.json(result);
+							}
+						);
+					});
+				}
+			});
+		});
+	});
+});
+
+app.listen(3000);
+console.log("Connect to http://127.0.0.1:3000/");
+
+
